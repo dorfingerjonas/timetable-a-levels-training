70f59690c30e0697d50d592a321b9fbbeaa96684
dorfingerjonas <contact@dorfingerjonas.at>
2022-03-06 23:21:26 +0100

    add websocket-server

---
+++ websocker-server/src/main/java/org/acme/websockets/ChatSocket.java
@@ -0,0 +1,54 @@
+package org.acme.websockets;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+
+import org.jboss.logging.Logger;
+
+@ServerEndpoint("/ws/{username}")
+@ApplicationScoped
+public class ChatSocket {
+
+    private static final Logger LOG = Logger.getLogger(ChatSocket.class);
+
+    Map<String, Session> sessions = new ConcurrentHashMap<>();
+
+    @OnOpen
+    public void onOpen(Session session, @PathParam("username") String username) {
+        sessions.put(username, session);
+    }
+
+    @OnError
+    public void onError(Session session, @PathParam("username") String username, Throwable throwable) {
+        sessions.remove(username);
+        LOG.error("onError", throwable);
+        broadcast("User " + username + " left on error: " + throwable);
+    }
+
+    @OnMessage
+    public void onMessage(String message) {
+        if (!message.equalsIgnoreCase("_ready_")) {
+            broadcast(message);
+        }
+    }
+
+    private void broadcast(String message) {
+        sessions.values().forEach(s -> {
+            s.getAsyncRemote().sendObject(message, result -> {
+                if (result.getException() != null) {
+                    System.out.println("Unable to send message: " + result.getException());
+                }
+            });
+        });
+    }
+
+}
