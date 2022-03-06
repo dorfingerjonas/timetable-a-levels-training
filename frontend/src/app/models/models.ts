8211e560d63f027b1800ebc9e8728d7240e68a15
dorfingerjonas <contact@dorfingerjonas.at>
2022-03-06 14:28:18 +0100

    implement models

---
+++ frontend/src/app/models/models.ts
@@ -0,0 +1,21 @@
+export interface Class {
+  id: string;
+  room: string;
+}
+
+export interface Teacher {
+  id: number;
+  firstName: string;
+  lastName: string;
+  room: string;
+}
+
+export interface Unit {
+  id: number;
+  day: number;
+  unit: number;
+  subject: string;
+  teacher: Teacher;
+  schoolclass: Class | null;
+  hasChanged?: 'self' | 'other';
+}
